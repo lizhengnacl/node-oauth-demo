245ee3d807e6a61df1ca414c944ab90864a62e72
lizhengnacl <lizhengnacl@didiglobal.com>
2024-02-28 16:00:29 +0800

    feat: google

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Fill in your client ID and client secret that you obtained
 // while registering the application
-const clientID = '240beeadb32b08f2fcb5';
-const clientSecret = '9a9109e86e054ba1698c9bf9fd61db89a0dd9e33';
+const clientID = '953479267209-31r63sb2gbln6vm8lnoumf3bpd0jcjl4.apps.googleusercontent.com';
+const clientSecret = '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';
+const redirectUri = 'https://simpletalkai.com/google_oauth_demo/oauth/redirect'
 
 const Koa = require('koa');
 const path = require('path');
@@ -16,25 +17,34 @@ const main = serve(path.join(__dirname + '/public'));
 
 const oauth = async ctx => {
   const requestToken = ctx.request.query.code;
-  console.log(colors.red(`authorization code: ${requestToken}`));
+  // console.log('=========== ctx.request ===========', ctx.request);
+  // console.log('=========== ctx.request.query ===========', ctx.request.query);
+  console.log(colors.green(`authorization code: ${requestToken}`));
+
+  // console.log('=========== tokens ===========', tokens);
+  // console.log(colors.green(`access token: ${tokens}`));
+  // oauth2Client.setCredentials(tokens);
 
   const tokenResponse = await axios({
     method: 'post',
-    url: 'https://github.com/login/oauth/access_token?' +
+    url: 'https://oauth2.googleapis.com/token?' +
         `client_id=${clientID}&` +
         `client_secret=${clientSecret}&` +
-        `code=${requestToken}`,
+        `code=${requestToken}` +
+        `redirect_uri=${redirectUri}` +
+        `grant_type=authorization_code`,
     headers: {
       accept: 'application/json',
     },
   });
 
   const accessToken = tokenResponse.data.access_token;
-  console.log(colors.red(`access token: ${accessToken}`));
+  console.log(colors.green(`access token: ${accessToken}`));
+
 
   const result = await axios({
     method: 'get',
-    url: `https://api.github.com/user`,
+    url: `https://www.googleapis.com/oauth2/v1/userinfo`,
     headers: {
       accept: 'application/json',
       Authorization: `token ${accessToken}`,
@@ -53,5 +63,5 @@ const oauth = async ctx => {
 app.use(main);
 app.use(route.get('/oauth/redirect', oauth));
 
-app.listen(8080);
-console.log('=========== 8080 ===========');
+app.listen(8881);
+console.log(colors.green('=========== 8881 ==========='));
