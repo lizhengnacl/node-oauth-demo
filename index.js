e315bd39744823bbf0361438e3e48f0125d7eab4
lizhengnacl <lizhengnacl@didiglobal.com>
2024-02-28 11:55:28 +0800

    权限范围；输出颜色

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Fill in your client ID and client secret that you obtained
 // while registering the application
-const clientID = '7e015d8ce32370079895'
-const clientSecret = '2b976af0e6b6ceea2b1554aa31d1fe94ea692cd9'
+const clientID = '240beeadb32b08f2fcb5';
+const clientSecret = '9a9109e86e054ba1698c9bf9fd61db89a0dd9e33';
 
 const Koa = require('koa');
 const path = require('path');
 const serve = require('koa-static');
 const route = require('koa-route');
 const axios = require('axios');
+const colors = require('./util/colors');
 
 const app = new Koa();
 
@@ -15,37 +16,42 @@ const main = serve(path.join(__dirname + '/public'));
 
 const oauth = async ctx => {
   const requestToken = ctx.request.query.code;
-  console.log('authorization code:', requestToken);
+  console.log(colors.red(`authorization code: ${requestToken}`));
 
   const tokenResponse = await axios({
     method: 'post',
     url: 'https://github.com/login/oauth/access_token?' +
-      `client_id=${clientID}&` +
-      `client_secret=${clientSecret}&` +
-      `code=${requestToken}`,
+        `client_id=${clientID}&` +
+        `client_secret=${clientSecret}&` +
+        `code=${requestToken}`,
     headers: {
-      accept: 'application/json'
-    }
+      accept: 'application/json',
+    },
   });
 
   const accessToken = tokenResponse.data.access_token;
-  console.log(`access token: ${accessToken}`);
+  console.log(colors.red(`access token: ${accessToken}`));
 
   const result = await axios({
     method: 'get',
     url: `https://api.github.com/user`,
     headers: {
       accept: 'application/json',
-      Authorization: `token ${accessToken}`
-    }
+      Authorization: `token ${accessToken}`,
+    },
   });
   console.log(result.data);
   const name = result.data.name;
+  const id = result.data.id;
 
-  ctx.response.redirect(`/welcome.html?name=${name}`);
+  ctx.cookies.set('myCookie', 'cookieValue', {
+    httpOnly: true,
+  });
+  ctx.response.redirect(`/welcome.html?name=${name}&id=${id}`);
 };
 
 app.use(main);
 app.use(route.get('/oauth/redirect', oauth));
 
 app.listen(8080);
+console.log('=========== 8080 ===========');
